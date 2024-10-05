b49c55d6763ae08f00915220c6dfc4c8de0c7488
goeranha <goeranha@gmail.com>
2024-10-05 12:36:30 +0200

    Fikset Hero endelig

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import Hero from "../components/Hero";
+import React from "react";
+import Image from 'next/image';
+import KostioXlLogo from '../assets/kostio-xl-logo.png';
+import KostioBackpicture from '../assets/kostio-backpicture.png';
 
 export default async function Index() {
   return (
     <div>
-      <Hero />
+      <div className="flex flex-col gap-4 items-center">
+      <div className="flex gap-8 justify-center items-center flex-col">
+        <Image src={KostioBackpicture} alt="Kostio Logo" width={350} />
+        <Image src={KostioXlLogo} alt="Kostio Logo" width={200} />
+      </div>
+      <p className="text-md lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
+        Se hvor mye bilen egentlig koster!
+      </p>
+      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
+    </div>
       <main className="flex-1 flex flex-col gap-6 px-4">
         
       </main>
