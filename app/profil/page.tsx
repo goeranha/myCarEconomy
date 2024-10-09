d6d8f228e4846336b9ebd0ea752cf3a690f248d2
goeranha <goeranha@gmail.com>
2024-10-09 23:46:36 +0200

    fikse userprofile

--- app/profil/page.tsx
+++ app/profil/page.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
 
-interface UserProfileProps {
-    name: string;
-    email: string;
-    avatarUrl: string;
-}
-
-const UserProfile: React.FC<UserProfileProps> = ({ name, email, avatarUrl }) => {
+const UserProfile = () => {
     return (
-        <div className="user-profile">
-            <img src={avatarUrl} alt={`${name}'s avatar`} className="avatar" />
-            <h2>{name}</h2>
-            <p>{email}</p>
+        <div>
+            test
         </div>
     );
 };
